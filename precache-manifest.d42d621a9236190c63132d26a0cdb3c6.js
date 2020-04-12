1aba25892a5c73a3b1498b69f2a66c95b05f1ec6
Ricardo Brambila <ricardo.brambila.jsx@gmail.com>
2020-04-11 22:07:49 -0300

    Updates

---
+++ precache-manifest.d42d621a9236190c63132d26a0cdb3c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2ae27b0b285ad58ebb1fdfdf4fca88",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.5273a573bb852462167e.bundle.js"
+  },
+  {
+    "url": "runtime~main.5273a573bb852462167e.bundle.js"
+  },
+  {
+    "revision": "9bb2fc8f0e1ba41b626b17c078869f6b",
+    "url": "static/media/tweet-author.9bb2fc8f.jpg"
+  },
+  {
+    "url": "vendors~main.5273a573bb852462167e.bundle.js"
+  },
+  {
+    "url": "vendors~main.5273a573bb852462167e.bundle.js.LICENSE.txt"
+  }
+]);
